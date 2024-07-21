a3b1b14ff8b16fc73bbe43d2cab5a4a624d19673
PriRex <prirex@yandex.ru>
2024-07-21 15:04:32 +0500

    fff

--- NewsPaper/urls.py
+++ NewsPaper/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for NewsPortal project.
+URL configuration for NewsPaper project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from ckeditor_uploader.views import upload, browse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('pages/', include('django.contrib.flatpages.urls')),
-   path('', include('news.urls')),
-   path('accounts/', include('allauth.urls')),
-   path('__debug__/', include("debug_toolbar.urls")),
-]
+    path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('', include('board.urls')),
+    path('accounts/', include('allauth.urls')),
+    re_path(r'^upload/', login_required(upload), name='ckeditor_upload'),
+    re_path(r'^browse/', login_required(never_cache(browse)), name='ckeditor_browse'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
